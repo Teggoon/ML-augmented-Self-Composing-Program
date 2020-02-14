3543f445ea9665ade6887550960177c204d9bbce
Haocheng Li <49082800+Teggoon@users.noreply.github.com>
2020-02-13 23:12:23 -0800

    Added proper commenting to MIDI convert file

--- MIDIconvert.py
+++ MIDIconvert.py
@@ -4,6 +4,7 @@ from midiutil import MIDIFile
 import math
 import random
 
+#the corresponding pitches for each chord
 chordsReference = {
     "C": (60, 64, 67),
     "D": (62, 65, 69),
@@ -14,13 +15,16 @@ chordsReference = {
 }
 
 
+#written_chords.txt already contains the chords that were written by the machine
 file = open("written_chords.txt","r")
 
 chords = []
 
+#populating chords array with written chords
 for line in file:
     chords.append(line[0])
 
+#The pitches of the notes in the octave
 degrees  = [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
 track    = 0
 channel  = 0
@@ -30,21 +34,31 @@ tempo    = 400 + math.floor(random.random() * 100)   # In BPM
 volume   = 100  # 0-127, as per the MIDI standard
 key = -15 + math.floor(random.random() * 12)
 
+# Option to create dynamics in the arpeggio
 volumePack = [60,60,60,60,60,60,60,60]
 
 MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
                       # automatically)
 MyMIDI.addTempo(track, time, tempo)
 
+#How many times we want the arp loop to run
 loop = 4
 
+#Randomize how many octaves up we want to shift our arps
 layerUp = math.floor(random.random() * 2)
 
+#Actual design of the arp; this isn't randomized or written by machine, but designed manually.
 arpeggioLayer = [0, 2, 1, 0, 2, 1, 0, 1]
 
+#For each chord loop:
 for n in range(0, loop):
+    #for each chord:
     for c, i in enumerate(chords):
+        
+        #get current chord
         currentChord = chords[c];
+        
+        #Write out each note
         for v in range(0, len(volumePack)):
             if arpeggioLayer[v] == 1:
                 MyMIDI.addNote(track, channel, key + chordsReference[currentChord][1] + 12, time + c*8 + v + n * 32, duration, volumePack[v])
@@ -52,7 +66,7 @@ for n in range(0, loop):
                 MyMIDI.addNote(track, channel, key + chordsReference[currentChord][arpeggioLayer[v]], time + c*8 + v + n * 32, duration, volumePack[v])
 
 
-
+#Export to MIDI
 def printToMIDI():
     with open("generated_chords.mid", "wb") as output_file:
         MyMIDI.writeFile(output_file)
